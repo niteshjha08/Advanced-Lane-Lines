feeabf5c9183d3026a6b45d81ec2431ec7410362
Nitesh Jha <niteshj818@gmail.com>
2020-10-19 01:43:22 +0530

    Optimized Sobel Magnitude function. Implemented inverse perspective, and lane fill.
    To do:
    1. Implement sanity checking functions.
    2. Improve thresholding img, use HLS.

--- src/binary_tuner.py
+++ src/binary_tuner.py
@@ -23,6 +23,15 @@ def abs_sobel_mag(img,orient='x',threshold=(0,255),ksize=3):
     norm_sobel=np.uint8(255*abs_sobel/np.max(abs_sobel))
     binary_op=np.zeros_like(gray)
     binary_op[((norm_sobel>threshold[0]) & (norm_sobel<threshold[1]))]=255
+    return binary_op,sobel
+
+def get_sobel_mag(img,sobelx,sobely,thresh=(0,255),ksize=3):
+    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    abs_sobel_xy = np.sqrt(sobelx ** 2 + sobely ** 2)
+    norm_sobel = (255 * abs_sobel_xy / np.max(abs_sobel_xy)).astype(np.uint8)
+    binary_op = np.zeros_like(gray)
+    binary_op[(norm_sobel > thresh[0]) & (norm_sobel < thresh[1])] = 255
+
     return binary_op
 
 def mag_sobel(img,thresh=(0,255),ksize=3):
@@ -47,13 +56,48 @@ def dir_sobel(img,thresh=(0,255),ksize=3):
     binary_op[(norm_sobel > thresh[0]) & (norm_sobel < thresh[1])] = 255
     return binary_op
 
-def perspective_transform(image):
+def write_perspective_transform(image):
+    print('writing mtx')
     dst = np.array([[[300, 720]], [[980, 720]], [[980, 0]], [[300, 0]]])
     points = np.array([[[200, 720]], [[1100, 720]], [[685, 450]], [[595, 450]]])
     M = cv2.getPerspectiveTransform(points.astype(np.float32), dst.astype(np.float32))
     warpimg = cv2.warpPerspective(image, M, (image.shape[1], image.shape[0]), cv2.INTER_LINEAR)
-    # img = cv2.polylines(image, [points], True, (0, 255, 0), 1)
-    # img = cv2.polylines(image, [dst], True, (0, 0, 255), 1)
+    with open('perspective_mtx.pickle','wb') as f:
+      pickle.dump(M,f)
+    return warpimg
+
+def write_inv_perspective_transform(image):
+    print('writing mtx inv')
+    dst = np.array([[[300, 720]], [[980, 720]], [[980, 0]], [[300, 0]]])
+    points = np.array([[[200, 720]], [[1100, 720]], [[685, 450]], [[595, 450]]])
+    M = cv2.getPerspectiveTransform(dst.astype(np.float32), points.astype(np.float32))
+    warpimg = cv2.warpPerspective(image, M, (image.shape[1], image.shape[0]), cv2.INTER_LINEAR)
+    with open('inverse_perspective_mtx.pickle','wb') as f:
+      pickle.dump(M,f)
+    return warpimg
+
+
+def get_perspective_mtx():
+    print("reading mtx")
+    with open('perspective_mtx.pickle', 'rb') as f:
+        M = pickle.load(f)
+    return M
+
+
+def get_inv_perspective_mtx():
+    print("Reading inv mtx")
+    with open('inverse_perspective_mtx.pickle','rb') as f:
+        Minv = pickle.load(f)
+    return Minv
+
+
+def perspective_transform(img,M):
+    warpimg = cv2.warpPerspective(img, M, (img.shape[1], img.shape[0]), cv2.INTER_LINEAR)
+    return warpimg
+
+
+def inv_perspective_transform(img,Minv):
+    warpimg = cv2.warpPerspective(img, Minv, (img.shape[1], img.shape[0]), cv2.INTER_LINEAR)
     return warpimg
 
 
@@ -83,11 +127,16 @@ def pipeline(image):
         cv2.imshow('warp', cv2.resize(warpimg, (int(sobelx.shape[1] / 2), int(sobelx.shape[0] / 2))))
         cv2.imshow('orig', cv2.resize(orig, (int(sobelx.shape[1] / 2), int(sobelx.shape[0] / 2))))
 
-def undistort_img(img):
-    with open('calib_param.pickle', 'rb') as f:
-        [ret, mtx, dist, rvecs, tvecs] = pickle.load(f)
+def undistort_img(img,mtx,dist):
     undist = cv2.undistort(img, mtx, dist, None, None)
     return undist
+
+
+def get_distortion_measure():
+    print("getting dist measure")
+    with open('calib_param.pickle', 'rb') as f:
+        [ret, mtx, dist, rvecs, tvecs] = pickle.load(f)
+    return ret,mtx,dist,rvecs,tvecs
 
 def main():
     cv2.namedWindow('tune')
@@ -105,8 +154,30 @@ def main():
     undist=undistort_img(frame)
     pipeline(undist)
 
+def checkperspective():
+    frame = cv2.imread('./../test_images/straight_lines1.jpg')
+    warp = perspective_transform(frame)
+    revwarp = inv_perspective_transform(warp)
+    cv2.imshow('warp', warp)
+    cv2.imshow('rev', revwarp)
+    cv2.imshow('frame', frame)
+    merge = cv2.addWeighted(frame, 0.5, revwarp, 0.5, 0)
+    cv2.imshow('merge', merge)
+    cv2.waitKey()
+
+def checknewfunctionmag():
+    image=cv2.imread('./../test_images/straight_lines1.jpg')
+    sobelx,actualsobelx = abs_sobel_mag(image, 'x', (28,153))
+    sobely,actualsobely = abs_sobel_mag(image, 'y', (49,211))
+    mag_sob = get_sobel_mag(image, actualsobelx,actualsobely,(40, 206))
+    mag2=mag_sobel(image,(40,206))
+    cv2.imshow('mag2',mag2)
+    cv2.imshow('mag1',mag_sob)
+    cv2.waitKey()
 
 if __name__=='__main__':
-    main()
+    #main()
+    print("running main")
+    checknewfunctionmag()
 
 
--- src/detect_lane.py
+++ src/detect_lane.py
@@ -1,20 +1,22 @@
 import cv2
 import numpy as np
 import pickle
-from binary_tuner import mag_sobel,abs_sobel_mag, dir_sobel, perspective_transform, undistort_img
+from binary_tuner import mag_sobel,abs_sobel_mag, dir_sobel, perspective_transform, inv_perspective_transform, undistort_img, get_perspective_mtx,\
+                        get_inv_perspective_mtx, get_distortion_measure, get_sobel_mag
+import time
 import matplotlib.pyplot as plt
-
-with open('calib_param.pickle','rb') as f:
-    [ret,mtx,dist,rvecs,tvecs]=pickle.load(f)
+from moviepy.editor import VideoFileClip
 
 
 def get_binary(img):
-    sobelx=abs_sobel_mag(img,'x',(28,153))
-    sobely=abs_sobel_mag(img,'y',(49,211))
-    mag_sob=mag_sobel(img,(40,206))
-    dir_sob=dir_sobel(img,(47,144))
+    sobelx,actualsobelx=abs_sobel_mag(img,'x',(28,153))
+    sobely,actualsobely=abs_sobel_mag(img,'y',(49,211))
+    #mag_sob=mag_sobel(img,(40,206))
+    mag_sob=get_sobel_mag(img,actualsobelx,actualsobely,(40,206))
+    #dir_sob=dir_sobel(img,(47,144))
     bin_img=np.zeros_like(sobelx)
-    bin_img[((sobelx==255) & (sobely==255))& ((dir_sob==255) | (mag_sob==255))]=255
+    #bin_img[((sobelx==255) & (sobely==255))& ((dir_sob==255) | (mag_sob==255))]=255
+    bin_img[((sobelx == 255) & (sobely == 255)) & (mag_sob == 255)] = 255
     return bin_img
 
 def get_histogram(img):
@@ -22,20 +24,64 @@ def get_histogram(img):
     histogram=np.sum(half_img,axis=0)
     return histogram
 
-def main():
-    img=cv2.imread('./../test_images/test2.jpg')
-    undist=undistort_img(img)
+def process_img(img):
+    #img=cv2.imread('./../test_images/test2.jpg')
+    undist=undistort_img(img,mtx,dist)
     bin_img=get_binary(undist)
-    warped=perspective_transform(bin_img)
+    warped=perspective_transform(bin_img,M)
     # draw_boxes(warped)
     leftx,lefty,rightx,righty,imgbox = sliding_window(warped)
-    imgcolor,R_left,R_right=fit_line(leftx,lefty,rightx,righty,imgbox)
-    cv2.putText(imgcolor,"Radius_left:{}".format(R_left),(img.shape[1]//4,img.shape[0]//2),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
-    cv2.putText(imgcolor, "Radius_right:{}".format(R_right), ( 3*img.shape[1] // 4,img.shape[0] // 2),
+    box=img.copy()
+    imgcolor,R_left,R_right,left_pairs,right_pairs=fit_line(leftx,lefty,rightx,righty,imgbox)
+    filled=fill_lane(left_pairs,right_pairs,imgcolor)
+    filled_rev_warp=inv_perspective_transform(filled,Minv)
+    final=cv2.addWeighted(box,0.8,filled_rev_warp,0.5,0)
+    cv2.putText(final, "Radius_left:{}".format(R_left), (img.shape[1] // 4, img.shape[0] // 2),
                 cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-    cv2.imshow('fit', imgcolor)
-    cv2.waitKey()
+    cv2.putText(final, "Radius_right:{}".format(R_right), (3 * img.shape[1] // 4, img.shape[0] // 2),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+    #cv2.imshow('filled',final)
+    return final
 
+
+
+def process_video():
+    cap=cv2.VideoCapture('./../project_video.mp4')
+    M=get_perspective_mtx()
+    Minv=get_inv_perspective_mtx()
+    ret,mtx,dist,rvecs,tvecs=get_distortion_measure()
+    while(cv2.waitKey(1)!=ord('q')):
+        ret,img=cap.read()
+        process_img(img,M,Minv,mtx,dist)
+
+M = get_perspective_mtx()
+Minv = get_inv_perspective_mtx()
+ret, mtx, dist, rvecs, tvecs = get_distortion_measure()
+
+def timepass(img):
+    cv2.putText(img,"Nitesh",(img.shape[1]//2,img.shape[0]//2),cv2.FONT_HERSHEY_SIMPLEX,2,(0,0,255),1)
+    return img
+
+
+def get_video():
+    videoloc='./../project_video.mp4'
+    white_output='myoutput.mp4'
+
+    clip1 = VideoFileClip("./../project_video.mp4")
+    white_clip = clip1.fl_image(process_img)  # NOTE: this function expects color images!!
+    white_clip.write_videofile(white_output, audio=False)
+
+def check():
+    img = cv2.imread('./../test_images/test2.jpg')
+    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    warped = perspective_transform(gray)
+    revwarp= inv_perspective_transform(warped)
+    merged=cv2.addWeighted(gray,0.5,revwarp,0.5,0)
+    cv2.imshow('orig',gray)
+    cv2.imshow('rev',revwarp)
+    cv2.imshow('merge',merged)
+
+    cv2.waitKey()
 def draw_boxes(img):
     margin=100
     nwindows=9
@@ -74,7 +120,6 @@ def sliding_window(img):
 
     hist=get_histogram(img)
     midpoint = hist.shape[0] // 2
-    print("mid", midpoint)
     leftx_base=np.argmax(hist[:midpoint])
     rightx_base=midpoint + np.argmax(hist[midpoint:])
     imgcolor=np.dstack((img,img,img))
@@ -96,8 +141,7 @@ def sliding_window(img):
 
         right_x_low = rightx_current - margin
         right_x_high = rightx_current + margin
-        print((left_x_low,y_low),(left_x_high,y_high))
-        print((right_x_low, y_low), (right_x_high, y_high))
+
         cv2.rectangle(imgcolor,(left_x_low,y_low),(left_x_high,y_high),(0,0,255),2)
         cv2.rectangle(imgcolor, (right_x_low, y_low), (right_x_high, y_high), (255, 0, 0), 2)
 
@@ -108,13 +152,10 @@ def sliding_window(img):
 
         right_nonzeros_in_window = ((nonzerox >= right_x_low) & (nonzerox <= right_x_high) & (nonzeroy >= y_low)
                                            & (nonzeroy < y_high)).nonzero()[0]
-        print(len(left_nonzeros_in_window))
-        print(len(right_nonzeros_in_window))
+
         if(len(left_nonzeros_in_window)>minpix):
-            print("updating left")
             leftx_current= np.int(np.mean(nonzerox[left_nonzeros_in_window]))
         if(len(right_nonzeros_in_window)>minpix):
-            print("updating right")
             rightx_current= np.int(np.mean(nonzerox[right_nonzeros_in_window]))
 
         left_indices.append(left_nonzeros_in_window)
@@ -136,14 +177,11 @@ def fit_line(leftx,lefty,rightx,righty,img):
     #imgcolor=np.dstack((img,img,img))
     left_coeff=np.polyfit(lefty,leftx,2)
     right_coeff=np.polyfit(righty,rightx,2)
-    #left_min=np.min(leftx)
-    #left_max=np.max(leftx)
 
     lefty_arr=np.linspace(0,img.shape[1]-1,img.shape[1])
     leftx_arr=left_coeff[0]* (lefty_arr**2) + left_coeff[1]*lefty_arr + left_coeff[2]
     left_pairs=[(leftx_arr[i],lefty_arr[i]) for i in range(len(leftx_arr))]
-   # right_min = np.min(rightx)
-    #right_max = np.max(rightx)
+
     righty_arr = np.linspace(0, img.shape[1]-1, img.shape[1])
     rightx_arr = right_coeff[0] * (righty_arr ** 2) + right_coeff[1] * righty_arr + right_coeff[2]
     right_pairs = [(rightx_arr[i], righty_arr[i]) for i in range(len(rightx_arr))]
@@ -152,17 +190,28 @@ def fit_line(leftx,lefty,rightx,righty,img):
     left_pairs=left_pairs.reshape(-1,1,2)
     right_pairs=np.array(right_pairs).astype(np.int32)
     right_pairs=right_pairs.reshape(-1,1,2)
+
     cv2.polylines(img,[left_pairs],False,(0,255,0),5)
     cv2.polylines(img,[right_pairs],False,(0,255,0),5)
     R_left=get_rad_curvature(left_coeff[0],left_coeff[1],img.shape[0])
     R_right=get_rad_curvature(right_coeff[0],right_coeff[1],img.shape[0])
 
-    return img,R_left,R_right
+    return img,R_left,R_right,left_pairs,right_pairs
+
 
 def get_rad_curvature(A,B,y):
     R=(1+(2*A*y + B)**2)**(3/2)/(2*A)
     return R
 
 
+def fill_lane(left_pairs,right_pairs,img):
+    pts = np.array([left_pairs, np.flipud(right_pairs)])
+    pts = np.concatenate(pts)
+    cv2.fillPoly(img, [pts], (0, 255, 0))
+    return img
+
+
 if __name__=='__main__':
-    main()
+    #process_video()
+    # check()
+    get_video()
