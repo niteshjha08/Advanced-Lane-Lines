6855152fda3306ea41cb840ded04092e10ae19ce
Nitesh Jha <niteshj818@gmail.com>
2020-10-24 18:43:47 +0530

    Completed search_around_poly() and added visualisation for the search areas

--- src/detect_lane.py
+++ src/detect_lane.py
@@ -5,9 +5,15 @@ from binary_tuner import mag_sobel,abs_sobel_mag, dir_sobel, get_sobel_mag, hls_
 from perspective_transformations import perspective_transform, inv_perspective_transform, undistort_img, get_perspective_mtx,\
                         get_inv_perspective_mtx, get_distortion_measure
 import time
-import matplotlib.pyplot as plt
 from moviepy.editor import VideoFileClip
 
+
+M = get_perspective_mtx()
+Minv = get_inv_perspective_mtx()
+ret, mtx, dist, rvecs, tvecs = get_distortion_measure()
+left_coeff=np.zeros((3,1))
+right_coeff=np.zeros((3,1))
+first_run=True
 
 def get_binary(img):
     #sobelx,actualsobelx=abs_sobel_mag(img,'x',(40,153))
@@ -29,6 +35,7 @@ def get_binary(img):
     bin_img[(mag_sob == 255)| ((color_bin == 255))] = 255
     return bin_img
 
+
 def get_histogram(img):
     half_img=img[int(img.shape[0]/2):,:]
     histogram=np.sum(half_img,axis=0)
@@ -42,14 +49,11 @@ def process_video():
         ret,img=cap.read()
         process_img(img)
 
-M = get_perspective_mtx()
-Minv = get_inv_perspective_mtx()
-ret, mtx, dist, rvecs, tvecs = get_distortion_measure()
 
 
 def get_video():
     videoloc='./../project_video.mp4'
-    white_output='updated_video.mp4'
+    white_output='previousfit_video.mp4'
 
     clip1 = VideoFileClip("./../project_video.mp4")
     white_clip = clip1.fl_image(process_img)  # NOTE: this function expects color images!!
@@ -83,6 +87,8 @@ def draw_boxes(img):
     cv2.waitKey()
 
 def sliding_window(img):
+    global left_coeff,right_coeff
+    print("Searching for the first time")
     left_indices=[]   # Save white pixels on left half of image
     right_indices=[]  # Save white pixels on right half of image
     margin=100
@@ -149,9 +155,14 @@ def sliding_window(img):
 
 # This function takes input from one image which has been fit with curve(sliding_window-->fit_line-->search_around_poly-->fit_line-->search_around_poly...)
 def search_around_poly(img):
+    print("Searching around previous fit")
+    global left_coeff,right_coeff
+    print("left coeff:",left_coeff)
+    print("right_coeff:",right_coeff)
     nonzero=img.nonzero()
-    nonzerox=nonzero[0]
-    nonzeroy=nonzero[1]
+    nonzerox=nonzero[1]
+    nonzeroy=nonzero[0]
+    margin = 100
 
     left_lane_inds=((nonzerox > nonzeroy**2*left_coeff[0] + nonzeroy*left_coeff[1]+left_coeff[2]-margin)&\
                    (nonzerox < nonzeroy**2*left_coeff[0] + nonzeroy*left_coeff[1]+left_coeff[2]+margin)).nonzero()[0]
@@ -163,13 +174,52 @@ def search_around_poly(img):
     rightx=nonzerox[right_lane_inds]
     righty=nonzeroy[right_lane_inds]
     # Now send it to fit_line which will give new coefficients of fit. Then use it again for searching lanes in the next frame.
+    return leftx,lefty,rightx,righty,img
 
+
+def draw_current_lanes(img):
+    global left_coeff,right_coeff
+    ys=np.linspace(0,img.shape[0]-1,img.shape[0])
+    xleft=ys**2*left_coeff[0]+ys*left_coeff[1]+left_coeff[2]
+    xright=ys**2*right_coeff[0]+ys*right_coeff[1]+right_coeff[2]
+    left_pairs = [(xleft[i], ys[i]) for i in range(len(xleft))]
+    right_pairs = [(xright[i], ys[i]) for i in range(len(xright))]
+    left_pairs = np.array(left_pairs).astype(np.int32)
+    left_pairs = left_pairs.reshape(-1, 1, 2)
+    right_pairs = np.array(right_pairs).astype(np.int32)
+    right_pairs = right_pairs.reshape(-1, 1, 2)
+
+    blank = np.zeros_like(img)
+    blank_color = np.dstack((blank, blank, blank))
+    left_lmargin=np.copy(left_pairs).T
+    left_rmargin = np.copy(left_pairs).T
+    left_lmargin[0]=left_lmargin[0]-100
+    left_lmargin = left_lmargin.T
+    left_rmargin[0]=left_rmargin[0]+100
+    left_rmargin=left_rmargin.T
+
+    right_lmargin = np.copy(right_pairs).T
+    right_rmargin = np.copy(right_pairs).T
+    right_lmargin[0] = right_lmargin[0] - 100
+    right_lmargin = right_lmargin.T
+    right_rmargin[0] = right_rmargin[0] + 100
+    right_rmargin = right_rmargin.T
+    left_margin=fill_lane(left_lmargin,left_rmargin,blank_color,(0,0,255))
+    both_margins=fill_lane(right_lmargin,right_rmargin,left_margin,(0,0,255))
+    cv2.imshow('left lanes leftright',both_margins)
+    cv2.waitKey()
+
+    return both_margins
 
 
 def fit_line(leftx,lefty,rightx,righty,img):
     #imgcolor=np.dstack((img,img,img))
+    global left_coeff, right_coeff
+    print(lefty)
+    print(rightx)
     left_coeff=np.polyfit(lefty,leftx,2)
     right_coeff=np.polyfit(righty,rightx,2)
+
 
     lefty_arr=np.linspace(0,img.shape[1]-1,img.shape[1])
     leftx_arr=left_coeff[0]* (lefty_arr**2) + left_coeff[1]*lefty_arr + left_coeff[2]
@@ -197,14 +247,17 @@ def get_rad_curvature(A,B,y):
     return R
 
 
-def fill_lane(left_pairs,right_pairs,img):
+
+
+def fill_lane(left_pairs,right_pairs,img,color=(0,255,0)):
     pts = np.array([left_pairs, np.flipud(right_pairs)])
     pts = np.concatenate(pts)
-    cv2.fillPoly(img, [pts], (0, 255, 0))
+    cv2.fillPoly(img, [pts], color)
     #cv2.imshow('filllane',img)
     return img
 
 def process_img(img):
+    global first_run
     cv2.imshow('actual input',img)
     #img=cv2.imread('./../test_images/test2.jpg')
     img=cv2.cvtColor(img,cv2.COLOR_RGB2BGR)
@@ -213,29 +266,36 @@ def process_img(img):
     bin_img=get_binary(undist)
     blank_img = np.zeros_like(bin_img)
     blank_color=np.dstack((blank_img,blank_img,blank_img))
-    print(blank_img.shape)
     warped=perspective_transform(bin_img,M)
-    leftx,lefty,rightx,righty,imgbox = sliding_window(warped)
-    box=undist.copy()
-    print(box.shape)
+    box = undist.copy()
+    if(first_run):
+        leftx,lefty,rightx,righty,imgbox = sliding_window(warped)
+    else:
+        search_margin=draw_current_lanes(warped)
+        cv2.imshow('search',search_margin)
+        leftx,lefty,rightx,righty,imgbox = search_around_poly(warped)
+
     imgcolor,R_left,R_right,left_pairs,right_pairs=fit_line(leftx,lefty,rightx,righty,imgbox)
     Rad_curve=(R_left+R_right)/2
     filled=fill_lane(left_pairs,right_pairs,blank_color)
+    cv2.imshow("this is filled result",filled)
     filled_rev_warp=inv_perspective_transform(filled,Minv)
-    final=cv2.addWeighted(box,1,filled_rev_warp,0.3,0)
+    final = cv2.addWeighted(box, 1, filled_rev_warp, 0.3, 0)
+    if(first_run==False):
+        search_margin_persp = inv_perspective_transform(search_margin, Minv)
+        cv2.imshow('search_margin',search_margin_persp)
+        final=cv2.addWeighted(final,1,search_margin_persp,0.4,0)
     cv2.putText(final, "Radius of curvature:{:.2f}".format(Rad_curve), (10, 30),
                 cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
     final=cv2.cvtColor(final,cv2.COLOR_BGR2RGB)
     cv2.imshow('filled223', final)
+    first_run=False
+    cv2.waitKey()
     return final
 
 
 if __name__=='__main__':
-     #process_video()
-    # check()
-    #
+     process_video()
      # frame=cv2.imread('./new_tests/test_img25.jpg')
      # final=process_img(frame)
-     # #cv2.imshow('final',final)
-     # cv2.waitKey()
-    get_video()
+     #get_video()
